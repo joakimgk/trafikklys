e02213f33ea6348b8025dda282c8e5cb76873f59
Joakim <joakim.grahl@gmail.com>
2018-12-05 18:03:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -15,7 +15,6 @@
 # define SLOW 1000
 # define RAPID 250
 
-
 void send_char(char c)
 {
 	while ((UCSRA & (1 << UDRE)) == 0) {};
@@ -31,8 +30,11 @@ void send_string(char *s)
 	}
 }
 
-volatile int i = 0;
+
+volatile int length = 0;
+volatile int rec = 0;
 volatile int step = 0;
+volatile int tempo = RAPID;
 volatile unsigned char program[256];
 volatile unsigned char ID;
 volatile unsigned int ID_mottatt;
@@ -94,17 +96,22 @@ int main(void){
 	
 	step = 0;
 	while (1) {
-		if (step < i) {
-			PORTB = program[step++];
-		} else {
-			//send_char('.');
-		}
-		_delay_ms(SLOW);
+		if ((step >= length) || (step == 256)) step = 0;
+		
+		PORTB = program[step++];
+		
+		my_delay_ms(tempo);
 	}
 	return 1;
 }
 
-
+void my_delay_ms(int n) {
+	int mem = tempo;
+	while(n--) {
+		if (mem != tempo) break;
+		_delay_ms(1);
+	}
+}
 
 ISR ( USART_RXC_vect , ISR_BLOCK )
 {
@@ -126,14 +133,34 @@ ISR ( USART_RXC_vect , ISR_BLOCK )
 		if ((ReceivedByte & 0b11100000) == ID) {
 			//send_char('j');
 			
-			if ((ReceivedByte & 0b00011111) == 0b00011111) { 
-				i = 0; // RESET: Prepare for new block of data (program)
+			//char resetPattern = (0b00011111 & 0b00011111);
+			
+			if ((ReceivedByte & 0b00000011) == 0b00000011) { 
+				length = 0; // RESET: Prepare for new block of data (program)
 				step = 0;
+				rec = 0;
 				//send_char('r');
+
 			} else {
 				//send_char('b');
 				payload = (ReceivedByte & 0b00011100) >> 2;
-				program[i++] = (payload ^ 0b11111111);  // inverted logic
+				
+				char a = (payload & 0b00000001 == 0b00000001 ? '1' : '0');
+				char b = (payload & 0b00000010 == 0b00000010 ? '1' : '0');
+				char c = (payload & 0b00000100 == 0b00000100 ? '1' : '0');
+				char tmp[6] = {rec+48,':',a,b,c, 0x00};
+				send_string(tmp);
+				
+				program[rec++] = (payload ^ 0b11111111);  // inverted logic
+				
+				if (rec >= 256) rec = 0;
+				else ++length;
+			}
+		} else {
+			// common reception stuff:
+			if ((ReceivedByte & 0b00000011) == 0b00000001) {
+				payload = (ReceivedByte & 0b11111100);
+				tempo = payload * 10;	
 			}
 		}
 	}
