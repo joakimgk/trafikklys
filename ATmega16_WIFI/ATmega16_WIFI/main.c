9f17ce43024769ed4d5031e754e0632978a0d538
Joakim Knudsen <joakim.knudsen@webstep.no>
2019-06-17 21:38:06 +0200

    Fikset (resatt) main.c fra eksempelkode. Funker nå

--- ATmega16_WIFI/ATmega16_WIFI/main.c
+++ ATmega16_WIFI/ATmega16_WIFI/main.c
@@ -5,7 +5,7 @@
 */
 
 
-#define F_CPU 8000000UL			/* Define CPU Frequency: vi satt den til 8MHz */
+#define F_CPU 8000000UL			/* Define CPU Frequency e.g. here its Ext. 12MHz */
 #include <avr/io.h>					/* Include AVR std. library file */
 #include <util/delay.h>				/* Include Delay header file */
 #include <stdbool.h>				/* Include standard boolean library */
@@ -19,9 +19,6 @@
 
 #define DEFAULT_BUFFER_SIZE		160
 #define DEFAULT_TIMEOUT			10000
-
-# define SLOW 20000
-# define RAPID 5000
 
 /* Connection Mode */
 #define SINGLE					0
@@ -49,7 +46,6 @@
 #define SSID				"Xperia z"
 #define PASSWORD			"fleskeeske"
 
-# define BUFFER_LENGTH 125
 
 enum ESP8266_RESPONSE_STATUS{
 	ESP8266_RESPONSE_WAITING,
@@ -81,39 +77,6 @@ int8_t Response_Status;
 volatile int16_t Counter = 0, pointer = 0;
 uint32_t TimeOut = 0;
 char RESPONSE_BUFFER[DEFAULT_BUFFER_SIZE];
-
-// Blinkeprogramting
-// #define STATE_MACHINE
-
-enum PROGRAM_STATES
-{
-	START,
-	RUNNING,
-	GET_PROGRAM,
-	RUN_PROGRAM,
-	SWITCH_PROGRAM
-};
-
-enum COMANDS
-{
-	GET_TEMPO = 0x02,
-	NEW_PROGRAM = 0x03,
-	RESET = 0x04
-};
-
-// program buffers
-volatile unsigned char program_a[BUFFER_LENGTH];
-volatile unsigned char program_b[BUFFER_LENGTH];
-
-// length of current program
-volatile int length = 0;
-// length of received program
-volatile int rec_length = 0;
-// playback position
-volatile int step = 0;
-// playback delay (ms)
-volatile int tempo = RAPID;
-
 
 void Read_Response(char* _Expected_Response)
 {
@@ -294,17 +257,11 @@ uint8_t ESP8266_Start(uint8_t _ConnectionNumber, char* Domain, char* Port)
 	char _atCommand[60];
 	memset(_atCommand, 0, 60);
 	_atCommand[59] = 0;
-	
-	/*
-	if(SendATandExpectResponse("AT+CIPSTART=4,\"UDP\",\"192.168.101.110\",8080,1112,0"
-			SendATandExpectResponse(_atCommand, "\r\nOK\r\n>");
-*/
 
 	if(SendATandExpectResponse("AT+CIPMUX?", "CIPMUX:0"))
 		sprintf(_atCommand, "AT+CIPSTART=\"TCP\",\"%s\",%s", Domain, Port);
 	else
 		sprintf(_atCommand, "AT+CIPSTART=\"%d\",\"TCP\",\"%s\",%s", _ConnectionNumber, Domain, Port);
-
 
 	_startResponse = SendATandExpectResponse(_atCommand, "CONNECT\r\n");
 	if(!_startResponse)
@@ -368,41 +325,39 @@ ISR (USART_RXC_vect)
 	SREG = oldsrg;
 }
 
+# define BUFFER_LENGTH 125
+# define SLOW 20000
+# define RAPID 5000
+
+// program buffers
+volatile unsigned char program[BUFFER_LENGTH];
+
+// length of current program
+volatile int length = 0;
+// length of received program
+volatile int rec_length = 0;
+// playback position
+volatile int step = 0;
+// playback delay (ms)
+volatile int tempo = RAPID;
+
 ISR (TIMER1_OVF_vect)    // Timer1 ISR
 {
 	if ((step >= length) || (step == BUFFER_LENGTH)) step = 0;
-		
-	PORTB = program_a[step++];  // *(program + step++);
+	
+	PORTB = program[step++];  // *(program + step++);
 
 	TCNT1 = tempo;   // for 1 sec at 16 MHz
 }
-
-
-
-
-void sendHello()
- {
-	ESP8266_Send("Hei verden!");
-	 
- }
 
 void setupTimerISR()
 {
 	TCNT1 = tempo;   // for 1 sec at 16 MHz
 
 	TCCR1A = 0x00;
-	TCCR1B = (1<<CS10) | (1<<CS12);;  // Timer mode with 1024 prescler
+	TCCR1B = (1<<CS10) | (1<<CS12);;  // Timer mode with 1024 prescaler
 	TIMSK = (1 << TOIE1) ;   // Enable timer1 overflow interrupt(TOIE1)
 }
-
-void sendReady()
-{
-	char _buffer[150];
-	memset(_buffer, 0, 150);
-	sprintf(_buffer, "KLAR FOR PROGRAM\r\n");
-	ESP8266_Send(_buffer);
-}
-
 
 int main(void)
 {
@@ -411,81 +366,48 @@ int main(void)
 	#ifdef SEND_DEMO
 	uint8_t Sample = 0;
 	#endif
-
-	USART_Init(115200);						/* Initiate USART baud rate */
 	
-	//setupTimerISR();
-	
-	// create an initial program to keep the loop busy until first program is received (and started)
-	program_a[0] = 0xFF;
-	program_a[1] = 0x00;
-	length = 2;
-	step = 0;	
-	
-	sei();									/* Start global interrupt */
-	
-	USART_SendString("HEI VELKOMMEN VERDEN");
-	
-	// bruk LEDs for å indikere hvor langt programmet kommer før det evt kræsjer
 	DDRB = 0xFF; // set PORTB for output
 	PORTB = 0x00; // turn ON all LEDs initially (to indicate ready)
-	
-	while (!ESP8266_Begin());
-	
-	USART_SendString("ESP8266_Begin");
-	
-	PORTB = 0xFF;
-		
-	ESP8266_WIFIMode(BOTH_STATION_AND_ACCESPOINT);/* 3 = Both (AP and STA) */
-		
-	ESP8266_ConnectionMode(SINGLE);			/* 0 = Single; 1 = Multi */
-		
-	ESP8266_ApplicationMode(NORMAL);		/* 0 = Normal Mode; 1 = Transperant Mode */
-			
-	if(ESP8266_connected() == ESP8266_NOT_CONNECTED_TO_AP)			
-		ESP8266_JoinAccessPoint(SSID, PASSWORD);
-		
-	// get IP
-	_delay_ms(5000);	/* Thingspeak server delay */
-		
-	ESP8266_Start(0, DOMAIN, PORT);
 
-	USART_SendString("klar ...");
+	// create an initial program to keep the loop busy until first program is received (and started)
+	program[0] = 0xFF;
+	program[1] = 0x00;
+	length = 2;
+	step = 0;
 	
-	bool sentReady = false;
-	
-	
-	
+	USART_Init(115200);						/* Initiate USART with 115200 baud rate */
+	setupTimerISR();
+	sei();									/* Start global interrupt */
+
+	while(!ESP8266_Begin());
+	ESP8266_WIFIMode(BOTH_STATION_AND_ACCESPOINT);/* 3 = Both (AP and STA) */
+	ESP8266_ConnectionMode(SINGLE);			/* 0 = Single; 1 = Multi */
+	ESP8266_ApplicationMode(NORMAL);		/* 0 = Normal Mode; 1 = Transperant Mode */
+	if(ESP8266_connected() == ESP8266_NOT_CONNECTED_TO_AP)
+	ESP8266_JoinAccessPoint(SSID, PASSWORD);
+	ESP8266_Start(0, DOMAIN, PORT);
 	while(1)
 	{
-		_delay_ms(600);
-		
 		Connect_Status = ESP8266_connected();
 		if(Connect_Status == ESP8266_NOT_CONNECTED_TO_AP)
-			ESP8266_JoinAccessPoint(SSID, PASSWORD);
+		ESP8266_JoinAccessPoint(SSID, PASSWORD);
 		if(Connect_Status == ESP8266_TRANSMISSION_DISCONNECTED)
-			ESP8266_Start(0, DOMAIN, PORT);
-		
-		/*
-		if (!sentReady) {
-			//memset(_buffer, 0, 150*sizeof(char));
-			sprintf(_buffer, "GET /update?api_key=%s&field1=%d", API_WRITE_KEY, Sample++);
-			ESP8266_Send(_buffer);
-			_delay_ms(5000);	// Thingspeak server delay
-			sentReady = true;
-		}
-		*/
-		
+		ESP8266_Start(0, DOMAIN, PORT);
+
 		#ifdef SEND_DEMO
-			
-		memset(_buffer, 0, 50);
-		_buffer[49] = 0;
-
-		sprintf(_buffer, "test test");
+		memset(_buffer, 0, 150);
+		sprintf(_buffer, "GET /update?api_key=%s&field1=%d", API_WRITE_KEY, Sample++);
 		ESP8266_Send(_buffer);
-		_delay_ms(5000);	// Thingspeak server delay 
-
+		_delay_ms(15000);	/* Thingspeak server delay */
 		#endif
-
+		
+		#ifdef RECEIVE_DEMO
+		memset(_buffer, 0, 150);
+		sprintf(_buffer, "GET /channels/%s/feeds/last.txt", CHANNEL_ID);
+		ESP8266_Send(_buffer);
+		Read_Data(_buffer);
+		_delay_ms(600);
+		#endif
 	}
 }
