af64e3837db7be7abbfea0ef3fabc5acfe91776d
Joakim <joakim.grahl@gmail.com>
2019-07-18 00:11:26 +0200

    Update parse_data.c

--- parse_data.c
+++ parse_data.c
@@ -2,6 +2,13 @@
 #include <stdio.h>					/* Include standard IO library */
 #include <stdlib.h>	/* Include standard library */
 
+unsigned char program[150];
+unsigned char rec_program[150];
+unsigned char tmp[150];
+int length = 0;
+int rec_length = 0;
+int step = 0;
+
 void printBits(char a) {
     int i;
     for (i = 0; i < 8; i++) {
@@ -10,7 +17,19 @@ void printBits(char a) {
     printf("\n");
 }
 
+void swapArrays(char **a, char **b){
+  char *temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
 void handlePayload(char command, int len, char* payload) {
+	printf("\033[34mPROGRAM (%d bytes):\n", length);
+	for (int i = 0; i < length; i++) {
+		printBits(program[i]);
+	}
+	printf("\033[0m");
+			
     switch (command) {
         case 0x01:
             printf("\033[33mEndre TEMPO til %c\033[0m\n", payload[0]);
@@ -22,21 +41,29 @@ void handlePayload(char command, int len, char* payload) {
             
 		case 0x03:  // MOTTA PROGRAM  (dump _buffer inn i *program)
 			printf("\033[33mLast inn nytt PROGRAM (%d bytes)\033[0m\n", len);
+
+			memset(rec_program, 0, 150);
+			memcpy(rec_program, payload, len);
 			for (int i = 0; i < len; i++) {
-				printBits(payload[i]);
+				printBits(rec_program[i]);
 			}
+			rec_length = len;
 			break;
 
-/*
 		case 0x04: // BYTT PROGRAM
-			cur_program = program;
-			program = rec_program;
-			rec_program = cur_program;
+		    printf("\033[33mBytt PROGRAM\033[0m\n");
+		
+		    swapArrays(program, rec_program);
+		    /*
+			*tmp = *program;
+			*program = *rec_program;
+			*rec_program = *tmp;
+			*/
 			length = rec_length;
 			
 			step = 0;
 			break;
-*/
+
         default:
             break;
     }
@@ -44,6 +71,13 @@ void handlePayload(char command, int len, char* payload) {
 
 int main(void)
 {
+    
+    program[0] = 0xFF;
+	program[1] = 0x00;
+	length = 2;
+	step = 0;
+	
+	
 	char _buffer[150];
 	char * pbuffer_cmd;
 	unsigned char payload[10];
@@ -52,7 +86,11 @@ int main(void)
 	unsigned char nyttProgram[8] = {0x3F,0x9F,0xCF,0xE7,0xF3,0xF9,0xFC, 0};
 	
 	memset(_buffer, 0, 150);
-	sprintf(_buffer, "\r\nRecv 30 bytes\r\n+IPD,5:%u%u%s\r\nSEND OK\r\n\r\n+IPD,3:%u%u2\r\n", 0x03, 0x07, nyttProgram, 0x01, 0x01);
+	sprintf(_buffer, "\r\nRecv 30 bytes\r\n+IPD,5:%u%u%s\r\nSEND OK\r\n\r\n+IPD,3:%u%u2\r\n+IPD,3:%u%u\r\n\r\n+IPD,3:%u%u4\r\n", 
+	                0x03, 0x07, nyttProgram, 
+	                0x01, 0x01,
+	                0x04, 0x00,
+	                0x01, 0x01);
 	pbuffer_cmd = _buffer;
 	int t = 1;
 	while (1)
