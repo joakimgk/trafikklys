24eb9854c505cff91ff95084ec4ddf5701079f42
Joakim <joakim.grahl@gmail.com>
2019-12-06 23:49:51 +0100

    Update USART_RS232_C_file.c

--- ATmega16_WIFI/ATmega16_WIFI/USART_RS232_C_file.c
+++ ATmega16_WIFI/ATmega16_WIFI/USART_RS232_C_file.c
@@ -9,24 +9,24 @@
 void USART_Init(unsigned long BAUDRATE)				/* USART initialize function */
 {
 #ifdef DOUBLE_SPEED_MODE
-	UCSRA |=(1 << U2X);
+	UCSR0A |=(1 << U2X0);
 #endif
-	UCSRB |= (1 << RXEN) | (1 << TXEN) | (1 << RXCIE);	/* Enable USART transmitter and receiver */
-	UCSRC |= (1 << URSEL)| (1 << UCSZ0) | (1 << UCSZ1);	/* Write USCRC for 8 bit data and 1 stop bit */ 
-	UBRRL = BAUD_PRESCALE;							/* Load UBRRL with lower 8 bit of prescale value */
-	UBRRH = (BAUD_PRESCALE >> 8);					/* Load UBRRH with upper 8 bit of prescale value */
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);	/* Enable USART transmitter and receiver */
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);	/* Write USCRC for 8 bit data and 1 stop bit */ 
+	UBRR0L = BAUD_PRESCALE;							/* Load UBRRL with lower 8 bit of prescale value */
+	UBRR0H = (BAUD_PRESCALE >> 8);					/* Load UBRRH with upper 8 bit of prescale value */
 }
 
 char USART_RxChar()									/* Data receiving function */
 {
-	while (!(UCSRA & (1 << RXC)));					/* Wait until new data receive */
-	return(UDR);									/* Get and return received data */ 
+	while (!(UCSR0A & (1 << RXC0)));					/* Wait until new data receive */
+	return(UDR0);									/* Get and return received data */ 
 }
 
 void USART_TxChar(char data)						/* Data transmitting function */
 {
-	UDR = data;										/* Write data to be transmitting in UDR */
-	while (!(UCSRA & (1<<UDRE)));					/* Wait until data transmit and buffer get empty */
+	UDR0 = data;										/* Write data to be transmitting in UDR */
+	while (!(UCSR0A & (1<<UDRE0)));					/* Wait until data transmit and buffer get empty */
 }
 
 void USART_SendString(char *str)					/* Send string of USART data function */ 
