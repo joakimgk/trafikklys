829c837de6e96b7f38942f207aa586d8b6a491fa
Joakim <joakim.grahl@gmail.com>
2019-12-08 11:29:54 +0100

    Update blinketest.py

--- Python blinkeklient/blinketest.py
+++ Python blinkeklient/blinketest.py
@@ -7,7 +7,7 @@ print("Socket create")
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
+# Bind the socket to the port (IP is my PC)
 server_address = ('192.168.43.254', 10000)
 #print >>sys.stderr, 'starting up on %s port %s' % server_address
 print("Starter server på port")
@@ -22,6 +22,36 @@ while True:
     print('waiting for a connection')
 
     connection, client_address = sock.accept()
+	
+    program = [[
+    0b10000000,
+    0b10000000,
+    0b10000000,
+    0b10000000,
+    0b10000000,
+    0b10000000,
+    0b10000000,
+    0b10000000,
+    0b00100000,
+    0b00100000,
+    0b00100000,
+    0b00100000,
+    0b00100000,
+    0b00100000,
+    0b00100000,
+    0b00100000
+    ],
+    [
+    0b10000000,
+    0b11000000,
+    0b11100000,
+    0b11000000
+    ],
+    [
+    0b10100000,
+    0b01000000,
+    0b10100000
+    ]] 
 
     try:
 
@@ -30,13 +60,36 @@ while True:
         # Receive the data in small chunks and retransmit it
 			
         while True:
+
+            g = input("Tast 1-3 for å sende nytt program... ")
+            i = int(g) - 1
+            prog = program[i]
+            
+            Pakke = [3, len(program[i])] + program[i]
+            connection.sendall(bytes(Pakke))
+			
+            g = input("Tast for å sende nytt RESET... ")
+            Pakke = [4]
+            connection.sendall(bytes(Pakke))
+			
+			
 	     	
             g = input("Tast tempo: ");
             data = [1, 1, int(g)]
             Pakke = bytes(data)
-
+            #Pakke = b'\x01\x01' + bytes(g, encoding='utf8')   #\x32'  # cmd = 1 | len = 1 | payload = 50 (0x32)
+            #print (Pakke)
             connection.sendall(Pakke)
+			
+            #Pakke = '\x01\x01' + g;
+			#connection.sendall(Pakke.encode())
             print("Tempo sendt")
+	     	
+            #Pakke = '\x02\x01\x32'  # cmd = 2 | len = 1 
+            #input('Tast for å sende RESET... ')      # If you use Python 3
+            #connection.sendall(Pakke.encode())
+            #print("Reset sendt")
+	        
 
     finally:
         print('error!!')
